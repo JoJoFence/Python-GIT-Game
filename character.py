87ea3d92afe03ec9266f71a21dea40c1c393b762
mshafim <19mshafim@gmail.com>
2017-05-31 10:23:36 -0400

    Add files via upload

---
+++ character.py
@@ -0,0 +1,7 @@
+#  defines the player character and is based off the Being class
+import being
+
+
+class Human(being.Being):
+    def __init__(self, health, attack):
+        being.Being.__init__(self, health, attack)
