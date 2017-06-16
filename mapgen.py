4df7cdf81f05ae0bbc9a71a1fd5b6c994ea01b91
kason1212 <kason8@yahoo.com>
2017-06-16 10:58:53 -0400

    Add files via upload

--- mapgen.py
+++ mapgen.py
@@ -6,7 +6,7 @@ pygame.init()
 width = 512
 height = 512
 DISPLAYSURF = pygame.display.set_mode((width, height), 0, 32)
-pygame.display.set_caption('game')
+pygame.display.set_caption('Basically Tron')
 
 FPS = 30
 fpsClock = pygame.time.Clock()
@@ -24,41 +24,37 @@ lel = 2
 torf = True
 player1 = []
 player2 = []
+drct1 = "D"
+drct2 = "W"
 
-
-def movementhandler(): 
+def movementhandler():
+    global drct1
+    global drct2
+    global player1
+    global player2
     pygame.display.flip()
     if event.type == KEYDOWN:
         temp1 = [main.x, main.y]
         temp2 = [sec.x, sec.y]
         if temp1 not in player2 and temp1 not in player1[0:len(player1)-1]:
             if event.key == K_a:
-                main.x -= 5
+                drct1 = "A"
             elif event.key == K_d:
-                main.x += 5
+                drct1 = "D"
             elif event.key == K_w:
-                main.y -= 5
+                drct1 = "W"
             elif event.key == K_s:
-                main.y += 5
-            if not player1.__contains__([main.x, main.y]):
-                player1.append([main.x, main.y])
-            if len(player1) > 60:
-                DISPLAYSURF.blit(cover, player1[0])
-                player1.remove(player1[0])
+                drct1 = "S"
+
         if temp2 not in player1 and temp2 not in player2[0:len(player2)-1]:
             if event.key == K_LEFT:
-                sec.x -= 5
+                drct2 = "A"
             elif event.key == K_RIGHT:
-                sec.x += 5
+                drct2 = "D"
             elif event.key == K_UP:
-                sec.y -= 5
+                drct2 = "W"
             elif event.key == K_DOWN:
-                sec.y += 5
-            if not player2.__contains__([sec.x, sec.y]):
-                player2.append([sec.x, sec.y])
-            if len(player2) > 60:
-                DISPLAYSURF.blit(cover, player2[0])
-                player2.remove(player2[0])
+                drct2 = "S"
 
 
 while True:
@@ -71,6 +67,27 @@ while True:
             sys.exit()
 
         movementhandler()
-
+    if ([main.x,main.y]) not in player2:
+        if drct1 == "A":
+            main.x -= 5
+        elif drct1 == "D":
+            main.x += 5
+        elif drct1 == "W":
+            main.y -= 5
+        elif drct1 == "S":
+            main.y += 5
+    if ([sec.x, sec.y]) not in player1:
+        if drct2 == "A":
+            sec.x -= 5
+        elif drct2 == "D":
+            sec.x += 5
+        elif drct2 == "W":
+            sec.y -= 5
+        elif drct2 == "S":
+            sec.y += 5
+    if ([main.x, main.y]) not in player1:
+        player1.append([main.x, main.y])
+    if ([sec.x, sec.y]) not in player2:
+        player2.append([sec.x, sec.y])
     pygame.display.update()
     fpsClock.tick(FPS)
